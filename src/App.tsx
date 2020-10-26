a9b7b9f70d26cba3a345963bd0ae54cb4fb6237d
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-10-26 19:30:46 +0000

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
-import {Container} from "semantic-ui-react";
-import AuthNav from "./components/AuthNav";
+import {BrowserRouter as Router, Route} from "react-router-dom"
+import {Container, Loader} from "semantic-ui-react";
+import MenuBar from "./components/MenuBar";
+import {useAuth0} from "@auth0/auth0-react";
+
+import Profile from "./pages/Profile"
+
+function Home() {
+    return <div>Hello world</div>
+}
 
 function App() {
+    const {isLoading} = useAuth0();
+
+    if (isLoading) return <Loader active size='massive'/>;
+
     return (
-        <Container>
-            <div>Hello world</div>
-            <AuthNav/>
-        </Container>
+        <Router>
+            <Container>
+                <MenuBar/>
+                <Route path="/" exact component={Home}/>
+                <Route path="/profile" component={Profile}/>
+            </Container>
+        </Router>
     );
 }
 
