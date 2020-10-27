0740da37af1dae90888c0d9ed94693f5276c420b
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-10-27 17:10:07 +0000

    show all battle pokemons

---
+++ src/pages/PokemonSearch.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {useQuery} from "react-query";
+import {gql, request} from "graphql-request";
+import {Loader, Card } from "semantic-ui-react";
+import PokemonCard from "../components/PokemonCard";
+
+const endpoint: string = "https://graphql-pokemon2.vercel.app";
+
+function usePokemonNames() {
+    return useQuery("pokemonNames", async () => {
+
+        let {pokemons: data} = await request(
+            endpoint,
+            gql`
+                query {
+                    pokemons(first: 151) {
+                        number
+                        name
+                        image
+                        types
+                        evolutions {
+                            name
+                        }
+                    }
+                }
+            `
+        );
+        data = data.filter((pokemon:any)=>pokemon.evolutions === null)
+        return data;
+    }, {staleTime: 60_000});
+}
+
+
+
+const PokemonSearch = () => {
+    const {data, error, isLoading} = usePokemonNames();
+    // const [activeItem, setActiveItem] = useState('all')
+
+    if (isLoading) return <Loader active/>;
+
+    if (error) {
+        const {message}: any = error;
+        return <div>{message}</div>;
+    }
+    return (
+        <>
+            <Card.Group centered>
+                {data && data.map((pokemon: any) => (
+                    <PokemonCard pokemon={pokemon}/>
+                ))}
+            </Card.Group>
+        </>
+    );
+};
+
+export default PokemonSearch;
